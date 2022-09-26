b8e9e4411f8a8e36b8d182f8c536f6ea83bfbc36
Mingze Ma <mingzema.1998@foxmail.com>
2022-09-27 01:37:24 +0800

    feature(swagger): multi swagger doc config

---
+++ elearn-common/src/main/java/com/acs/elearn/common/config/BaseSwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.acs.elearn.common.config;
+
+import com.acs.elearn.common.domain.SwaggerProperties;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Basic Swagger Config
+ *
+ * @author Mingze Ma
+ */
+public abstract class BaseSwaggerConfig {
+
+    @Bean
+    public Docket createRestfulApi() {
+        SwaggerProperties swaggerProperties = swaggerProperties();
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo(swaggerProperties))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getApiBasePackage()))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(SwaggerProperties swaggerProperties) {
+        return new ApiInfoBuilder()
+                .title(swaggerProperties.getTitle())
+                .description(swaggerProperties.getDescription())
+                .contact(new Contact(swaggerProperties.getContactName(), swaggerProperties.getContactUrl(), swaggerProperties.getContactEmail()))
+                .version(swaggerProperties.getVersion())
+                .build();
+    }
+
+    /**
+     * Custom Swagger Properties
+     */
+    public abstract SwaggerProperties swaggerProperties();
+
+}
