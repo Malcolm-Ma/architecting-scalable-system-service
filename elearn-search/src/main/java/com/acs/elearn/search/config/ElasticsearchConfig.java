ea6c00e6589aa93738e791feec502206c46db107
Mingze Ma <mingzema.1998@foxmail.com>
2022-10-05 10:21:48 +0800

    feature(es): connect es server

---
+++ elearn-search/src/main/java/com/acs/elearn/search/config/ElasticsearchConfig.java
@@ -0,0 +1,43 @@
+package com.acs.elearn.search.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+/**
+ * @author Mingze Ma
+ */
+@Configuration
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.port}")
+    private int port;
+
+    @Value("${elasticsearch.connTimeout}")
+    private int connTimeout;
+
+    @Value("${elasticsearch.socketTimeout}")
+    private int socketTimeout;
+
+    @Value("${elasticsearch.connectionRequestTimeout}")
+    private int connectionRequestTimeout;
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(host + ":" + port)
+                .withConnectTimeout(connTimeout)
+                .withSocketTimeout(socketTimeout)
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
