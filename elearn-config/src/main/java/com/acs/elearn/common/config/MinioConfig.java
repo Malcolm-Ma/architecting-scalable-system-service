908dc7f8279adde28c96c0d1fec6fcd14bc11d1e
LessIsMo <lhj16398@163.com>
2022-10-15 19:01:11 +0800

    create minio branch

---
+++ elearn-config/src/main/java/com/acs/elearn/common/config/MinioConfig.java
@@ -0,0 +1,30 @@
+package com.acs.elearn.common.config;
+
+import com.acs.elearn.common.domain.MinioProperties;
+import io.minio.MinioClient;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableConfigurationProperties(MinioProperties.class)
+public class MinioConfig {
+
+    @Resource
+    private MinioProperties minioProperties;
+
+
+    /**
+     * 初始化 MinIO 客户端
+     */
+    @Bean
+    public MinioClient minioClient() {
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint(minioProperties.getEndpoint())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+        return minioClient;
+    }
+}
