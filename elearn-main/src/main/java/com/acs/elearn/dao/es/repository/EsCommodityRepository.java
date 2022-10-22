e73409e37af66b1cce17ebc698ca0784fe5d1018
Mingze Ma <mingzema.1998@foxmail.com>
2022-10-22 17:32:48 +0800

    bugfix(es): es application

--- elearn-main/src/main/java/com/acs/elearn/dao/es/repository/EsCommodityRepository.java
+++ elearn-main/src/main/java/com/acs/elearn/dao/es/repository/EsCommodityRepository.java
@@ -4,10 +4,12 @@ import com.acs.elearn.dao.es.model.EsCommodity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Mingze Ma
  */
+@Repository
 public interface EsCommodityRepository extends ElasticsearchRepository<EsCommodity, String> {
 
     Page<EsCommodity> findByCommodityNameOrCommodityIntroduction(String commodityName, String commodityIntroduction,
