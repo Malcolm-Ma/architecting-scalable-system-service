f3b85939fa9ec7ae91eaf6d6eb471af981183e04
Trekker-one <huangyuzhe2019@gmail.com>
2022-10-24 08:02:02 +0800

    feature: add API in path "/" for health checking purpose

---
+++ elearn-web/src/main/java/com/acs/elearn/web/controllers/CheckController.java
@@ -0,0 +1,21 @@
+package com.acs.elearn.web.controllers;
+
+import com.acs.elearn.common.response.ResponseHandler;
+import com.acs.elearn.common.response.model.ResponseModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class CheckController {
+
+    @GetMapping(path = "/")
+    @ResponseBody
+    ResponseEntity<ResponseModel<String>> get() {
+        return ResponseHandler.generateResponse("Health Check Success", HttpStatus.OK, null);
+    }
+}
